b78ddc52f493cb5072685aaab88eefab1f3c6cc1
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-04-18 19:00:34 +0100

    Modifications finales

--- src/main/java/net/zerhouani/pres/Pres2.java
+++ src/main/java/net/zerhouani/pres/Pres2.java
@@ -11,14 +11,14 @@ import java.util.Scanner;
 
 public class Pres2 {
     public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Scanner scanner = new Scanner(new File("src/main/java/net/zerhouani/pres/config.txt"));
+        Scanner scanner = new Scanner(new File("config.txt"));
         String daoClassName = scanner.nextLine(); // Lire l'implementation DAO
         Class cDao = Class.forName(daoClassName);
         IDao dao = (IDao) cDao.newInstance();
 
         String metierClassName = scanner.nextLine(); // Lire l'implementation Metier implementation
         Class cMetier = Class.forName(metierClassName);
-        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance();
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
         //IMetier metier = (IMetier) cMetier.getConstructor().newInstance();
         //Method setDao = cMetier.getDeclaredMethod("setDao", IDao.class);
         //setDao.invoke(metier,dao);
